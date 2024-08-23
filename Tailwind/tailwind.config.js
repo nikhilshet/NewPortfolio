6b59c026dcfa0b18f744e27604b6ea85430a4eea
nikhilshet <nikhilshet17@gmail.com>
2024-08-23 15:16:08 +0530

    Initial Commit

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/*.html", // Include HTML files from the dist directory
+  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
